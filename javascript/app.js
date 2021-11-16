b5389b5aafe306b980aa8c01e0e6beb03e774288
Soe Wai Yan Aung <soewaiyan392002@gmail.com>
2021-11-16 19:01:38 +0630

    add javascript remove books

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,98 @@
 let library = [];
 
-function Book(title, aurthor, isRead) {
+function Book(title, aurthor, read) {
   this.title = title;
   this.aurthor = aurthor;
-  this.isRead = isRead;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  let title = document.querySelector(".title-input").value;
-  let aurthor = document.querySelector(".aurthor-input").value;
-  let isRead = document.querySelector(".isRead").checked;
+function createBookCard(book) {
+  // Create Elements
+  const card = document.createElement("div");
+  const title = document.createElement("h1");
+  const aurthor = document.createElement("h3");
+  const read = document.createElement("input");
+  const remove = document.createElement("button");
+
+  // Add Attributes
+  card.className = "book-card";
+  title.className = "book-title";
+  aurthor.className = "book-aurthor";
+  read.className = "book-read btn read-toggle";
+  read.setAttribute("type", "checkbox");
+  remove.className = "book-remove btn";
+
+  // Take value from form and store
+  title.textContent = book.title;
+  aurthor.textContent = book.aurthor;
+  read.checked = book.read;
+  remove.textContent = "remove";
+
+  // Add elements to library UI
+  document.querySelector(".library-container").appendChild(card);
+  card.appendChild(title);
+  card.appendChild(aurthor);
+  card.appendChild(read);
+  card.appendChild(remove);
+
+  // Add events
+  remove.addEventListener("click", function () {
+    let title = this.parentElement.querySelector(".book-title").textContent;
+    let aurthor = this.parentElement.querySelector(".book-aurthor").textContent;
+    del(title, aurthor);
+  });
+}
+
+function del(title, aurthor) {
+  library = library.filter(function (b) {
+    return b.title !== title || b.aurthor !== aurthor;
+  });
+  reset();
+  showTotalBook();
+  showBooks();
+}
+
+function showTotalBook() {
+  let totalBooks = library.length;
+  document.querySelector(".total-books").textContent = totalBooks;
+}
+
+function showBooks() {
+  let totalBooks = library.length;
+  for (let i = 0; i < totalBooks; i++) {
+    createBookCard(library[i]);
+  }
+}
+
+function reset() {
+  //  Reset the whole library so that books won't multi
+  document.querySelector(".library-container").innerHTML = "";
+  //  Reset the form inputs
+  document.querySelector(".title-input").value = "";
+  document.querySelector(".aurthor-input").value = "";
+  document.querySelector(".isRead").checked = false;
+}
+
+// To check the book that if input is already exist in library or not
+function isExist(title, aurthor) {
+  let totalBooks = library.length;
+  for (let i = 0; i < totalBooks; i++) {
+    let book = library[i];
+    if (
+      book.title.toLowerCase() === title.toLowerCase() &&
+      book.aurthor.toLowerCase() === aurthor.toLowerCase()
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function addBookToLibrary(title, aurthor, read) {
   if (!title || !aurthor) {
     console.log("oh no");
   } else {
-    let book = new Book(title, aurthor, isRead);
+    let book = new Book(title, aurthor, read);
     library.push(book);
   }
 }
@@ -22,5 +101,13 @@ const submitBookBtn = document.querySelector(".submit-book");
 
 submitBookBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  addBookToLibrary();
+  let title = document.querySelector(".title-input").value;
+  let aurthor = document.querySelector(".aurthor-input").value;
+  let read = document.querySelector(".isRead").checked;
+  if (isExist(title, aurthor)) {
+    addBookToLibrary(title, aurthor, read);
+    reset();
+    showTotalBook();
+    showBooks();
+  }
 });
